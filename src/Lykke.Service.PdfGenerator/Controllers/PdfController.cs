dcc0dc4d46a91253d77a7a55ef4ee1ccda79f2b8
ozhe <oleg.zhelenkov@hotmail.com>
2017-08-04 00:04:28 +0300

    Initial commit

---
+++ src/Lykke.Service.PdfGenerator/Controllers/PdfController.cs
@@ -0,0 +1,67 @@
+﻿using Lykke.Service.PdfGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Microsoft.WindowsAzure.Storage; 
+using Microsoft.Azure;
+
+namespace Lykke.Service.PdfGenerator.Controllers
+{
+    [RoutePrefix("api/pdf")]
+    public class PdfController : ApiController
+    {
+        private readonly string _containerName;
+
+        public PdfController()
+        {
+            _containerName = CloudConfigurationManager.GetSetting("PdfBlobContainer");
+        }
+
+        [HttpPost]
+        [Route("generate")]
+        public async Task<IHttpActionResult> Generate(PdfGenerateModel model)
+        {
+            if (model == null || string.IsNullOrWhiteSpace(model.HtmlSource))
+                return BadRequest("Please provide HtmlSource data.");
+
+            try
+            {
+                byte[] data = null;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    var pdf = TheArtOfDev.HtmlRenderer.PdfSharp.PdfGenerator.GeneratePdf(model.HtmlSource, PdfSharp.PageSize.A4, 0);
+                    pdf.Save(ms);
+                    data = ms.ToArray();
+                }
+
+                var blobName = Guid.NewGuid().ToString();
+                await StoreDataAsync(data, blobName);
+                
+
+                return Json(new { BlobContainer = _containerName, BlobName = blobName });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private async Task StoreDataAsync(byte[] data, string blobName)
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("BlobConnectionString"));
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(_containerName);
+            container.CreateIfNotExists();
+            
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            await blockBlob.UploadFromByteArrayAsync(data, 0, data.Length);
+        }
+    }
+}
---
+++ src/Lykke.Service.PdfGenerator/Models/PdfGenerateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lykke.Service.PdfGenerator.Models
+{
+    public class PdfGenerateModel
+    {
+        public string HtmlSource { get; set; }
+    }
+}
