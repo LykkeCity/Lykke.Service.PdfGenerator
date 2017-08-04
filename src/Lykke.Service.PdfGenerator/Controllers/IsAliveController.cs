de3f49972c6fabe8b206532e756e905dbf88cb20
Oleksandr Lysenko <oleksandr.lysenko@pl.pwc.com>
2017-08-04 15:17:50 +0200

    IsAlive(), swagger

---
+++ src/Lykke.Service.PdfGenerator/Controllers/IsAliveController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Lykke.Service.PersonalData.Web.Controllers
+{
+    /// <summary>
+    /// Controller to test service is alive
+    /// </summary>
+    [RoutePrefix("api/isalive")]
+    public class IsAliveController : ApiController
+    {
+        /// <summary>
+        /// Checks service is alive
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IsAliveResponse Get()
+        {
+            return new IsAliveResponse
+            {
+                Version = this.GetType().Assembly.GetName().Version.ToString(),
+                Env = Environment.GetEnvironmentVariable("Env")
+            };
+        }
+
+        public class IsAliveResponse
+        {
+            public string Version { get; set; }
+            public string Env { get; set; }
+        }
+    }
+}
